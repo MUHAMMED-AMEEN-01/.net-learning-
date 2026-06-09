53f684466f929fd7e0f28d372c45269ba67f9eb8
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-09 14:11:33 +0530

    Add credentials

---
+++ credentials/credentials/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace credentials
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string user, pass;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            user = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            pass = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (user == "admin" && pass == "password")
+            {
+                label3.Text = "Login successfully";
+                label3.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                label3.Text = " invalid username and password";
+                label3.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
