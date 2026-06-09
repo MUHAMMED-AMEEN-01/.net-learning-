f22b4c13b8e884e74c41b89e23834712bdb20cd9
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-09 13:36:56 +0530

    add celesius to Farenheit

---
+++ Celesius to Fahrenheit/Celesius to Fahrenheit/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Celesius_to_Fahrenheit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float f, c;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            c = float.Parse(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f = (c * 9 / 5) + 32;
+            textBox2.Text = f.ToString();
+        }
+    }
+}
