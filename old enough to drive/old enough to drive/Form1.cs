5beaa2d93e7c6e15aed078a3c905b3956abfc677
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-09 13:59:04 +0530

    Add old enough to drive

---
+++ old enough to drive/old enough to drive/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace old_enough_to_drive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (a > 17)
+            {
+                textBox2.Text = "Eligible";
+            }
+            else
+            {
+                textBox2.Text = "Not Eligible";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+        }
+    }
+}
---
+++ old enough to drive/old enough to drive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace old_enough_to_drive
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
