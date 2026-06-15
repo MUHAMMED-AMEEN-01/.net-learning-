a815cbdaa0ea4a238e5a8228ff5ded4b42c8c55c
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-15 14:45:28 +0530

    1st array project in .net C#

---
+++ Arrays/Arrays/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int[] i = { 10, 12, 13, 23 };
+            for (int j = 0; j < 4; j++){
+                listBox1.Items.Add( i[j]);
+            }
+        }
+    }
+}
