a6d38224a803036150407999a6e6023eee5e8715
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-15 11:24:30 +0530

    did Old enough to drive using function

---
+++ OldEnoughToDriveWithFunction/OldEnoughToDriveWithFunction/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OldEnoughToDriveWithFunction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void isdrive()
+        {
+            int a = Convert.ToInt16(textBox1.Text);
+            if (a >= 18)
+            {
+                label2.Text = "Eligible to drive";
+            }
+            else
+            {
+                label2.Text = "not Eligible to drive";
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           isdrive();
+        }
+    }
+}
