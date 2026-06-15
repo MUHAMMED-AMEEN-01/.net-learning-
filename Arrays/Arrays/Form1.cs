594a2b3f11059d058035cca6361138eed685321c
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-15 15:13:12 +0530

    change array folder now that act while clicking aganist buttons

--- Arrays/Arrays/Form1.cs
+++ Arrays/Arrays/Form1.cs
@@ -14,10 +14,33 @@ namespace Arrays
         public Form1()
         {
             InitializeComponent();
-            int[] i = { 10, 12, 13, 23 };
-            for (int j = 0; j < 4; j++){
-                listBox1.Items.Add( i[j]);
+           
+        }
+
+        int[] i = { 10, 12, 13, 23 };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                listBox1.Items.Add(i[j]);
+        }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            while (j < 4)
+            {
+                listBox1.Items.Add(i[j]);
+                j++;
             }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
