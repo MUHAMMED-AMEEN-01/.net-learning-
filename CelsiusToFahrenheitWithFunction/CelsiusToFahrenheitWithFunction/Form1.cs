6ddaf262a8a0f522589999c040b5665a8f11f7f7
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-15 10:58:21 +0530

    did Celsius to Fahrenheit with function

---
+++ CelsiusToFahrenheitWithFunction/CelsiusToFahrenheitWithFunction/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CelsiusToFahrenheitWithFunction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void change(){
+            int c= int.Parse(textBox1.Text);
+            int f= (c*9/5)+32;
+            textBox2.Text=f.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            change();
+        }
+    }
+}
