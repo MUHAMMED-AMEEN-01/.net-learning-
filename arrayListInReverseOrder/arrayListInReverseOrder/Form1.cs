7b197d98ea5b025ee271ded72ffa56110c58f4f2
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-18 22:22:44 +0530

    did ArrayList in reverse order

---
+++ arrayListInReverseOrder/arrayListInReverseOrder/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace arrayListInReverseOrder
+{
+    public partial class Form1 : Form
+    {
+        ArrayList arr = new ArrayList() { 12, 45, 23, 65, 11 };
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (int a in arr)
+            {
+                listBox1.Items.Add(a);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                listBox2.Items.Add(arr[i]);
+            }
+        }
+    }
+}
