9a46904c68b12c0cd73c1d1a1a864c1f7598b754
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-18 20:18:24 +0530

    Add ArrayList And Find Sum of Elements

---
+++ addArrayListAndFindSum/addArrayListAndFindSum/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace addArrayListAndFindSum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        ArrayList arr = new ArrayList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                if (i < 5)
+                {
+                    arr.Add(numericUpDown1.Value);
+                    label4.Text = "Element Added";
+                    i++;
+                }
+                else
+                {
+                    label4.Text = "reach limit!! ";
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sum=0;
+            foreach (decimal a in arr)
+            {
+                sum += Convert.ToInt16(a);
+            }
+            textBox1.Text = sum.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int j = 0; j < i; j++)
+            {
+                listBox1.Items.Add(arr[j]);
+                
+            }
+
+        }
+    }
+}
