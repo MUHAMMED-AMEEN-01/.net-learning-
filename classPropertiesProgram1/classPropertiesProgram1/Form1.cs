4f7209827a027a039f01956aa5cedd1ffc628a49
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-22 14:37:48 +0530

    did class propertis

---
+++ classPropertiesProgram1/classPropertiesProgram1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace classPropertiesProgram1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            obj = new std();
+        }
+
+        std obj;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj.name = textBox1.Text;
+            obj.mark = int.Parse(textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = obj.name;
+            textBox2.Text = obj.mark.ToString();
+        }
+    }
+}
---
+++ classPropertiesProgram1/classPropertiesProgram1/std.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classPropertiesProgram1
+{
+    class std
+    {
+        //members
+
+        private string nm;
+        private int m1;
+
+        // properites
+
+        public string name
+        {
+            set{nm=value;}
+            get { return nm; }
+        }
+        public int mark
+        {
+            set { m1 = value; }
+            get { return m1; }
+        }
+    }
+}
