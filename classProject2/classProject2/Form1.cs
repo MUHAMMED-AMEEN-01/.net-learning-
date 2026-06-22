3e614a18067fe2b5cbb10f757166c092b0222a1c
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-22 13:39:51 +0530

    did classProgram2

---
+++ classProject2/classProject2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace classProject2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Std obj;
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj = new Std();
+            int m1=int.Parse(textBox2.Text);
+            int m2 = int.Parse(textBox3.Text);
+            obj.func(textBox1.Text,m1,m2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] a= obj.ret();
+            textBox1.Text = a[0];
+            textBox2.Text = a[1];
+            textBox3.Text = a[2];
+        }
+    }
+}
---
+++ classProject2/classProject2/Std.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classProject2
+{
+    class Std
+    {
+        private string nm;
+        private int m1, m2;
+        public void func(string name, int n1, int n2)
+        {
+            nm = name;
+            m1 = n1;
+            m2 = n2;
+        }
+        public string[] ret()
+        {
+            string[] ret={nm,m1.ToString(),m2.ToString()};
+            return ret;
+        }
+    }
+}
