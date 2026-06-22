bc5c429d8ca658927a5ca294ed9a6e24581293df
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-22 12:01:22 +0530

    Did first class project

---
+++ classProject1/classProject1/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classProject1
+{
+    class Student
+    {
+        public string name,m1,m2;
+    }
+}
