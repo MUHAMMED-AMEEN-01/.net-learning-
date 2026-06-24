80a7626ca4074221736f7350b1a4b7492168afc9
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-24 20:56:58 +0530

    did fisrt inheritance program

---
+++ inheritanceP1/inheritanceP1/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace inheritanceP1
+{
+    class Car : Vehicle
+    {
+        public string model;
+        public String st()
+        {
+           return start(model);
+        }
+    }
+}
---
+++ inheritanceP1/inheritanceP1/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace inheritanceP1
+{
+    class Vehicle
+    {
+        public string start(string v="")
+        {
+            return v + " engin start";
+        }
+        public string stop()
+        {
+            return "engin stop";
+        }
+    }
+}
