2e3bb3514ed0bb7a791a5333387984abc0a70b36
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-25 22:39:48 +0530

    did counstruct program [default consrtactor and parameterized constructor]

---
+++ constructerP1/constructerP1/constecter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace constructerP1
+{
+    class constecter
+    {
+        // constrecter is a function name is exactily class name
+        private string a;
+        //defalt constructer
+
+        public constecter()
+        {
+            a = "this is from a constructer";
+        }
+
+        //properties
+
+        public string ab
+        {
+            get { return a; }
+        }
+        
+        //parametariezed consrtuctor
+
+        public constecter(string b)
+        {
+            a = b;
+        }
+    }
+}
