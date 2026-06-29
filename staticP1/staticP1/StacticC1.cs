3345029e0396da8b762b3cf33f2c77c69f86c11e
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-29 12:37:51 +0530

    first static program

---
+++ staticP1/staticP1/StacticC1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace staticP1
+{
+    class StacticC1
+    {
+        //static methode or variable have't need create objects . It access truogh class name
+
+        public static int mark=0;
+        public static void marklist()
+        {
+            MessageBox.Show("Name:Ameen\nMark:" + mark);
+        }
+
+    }
+}
