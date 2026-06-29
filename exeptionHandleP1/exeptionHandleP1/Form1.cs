b079bda09558edd4e3f436260e73da7039e6dc69
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-06-29 13:01:53 +0530

    did Exeption Handle programe with static variable and methode

---
+++ exeptionHandleP1/exeptionHandleP1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace exeptionHandleP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //try catch 
+
+            try
+            {
+                marklist.name = textBox1.Text;
+                marklist.mark = int.Parse(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                marklist.name = "-";
+                marklist.mark = 0;
+            }
+            finally
+            {
+                marklist.markShow();
+            }
+
+            
+
+        }
+    }
+}
---
+++ exeptionHandleP1/exeptionHandleP1/marklist.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace exeptionHandleP1
+{
+    class marklist
+    {
+        public static string name;
+        public static int mark;
+
+        public static void markShow() {
+            MessageBox.Show("Name: " + name + "\nMark: " + mark);
+        }
+    }
+}
