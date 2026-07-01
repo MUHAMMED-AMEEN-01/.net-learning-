d89c3f9b7849f5fc5437b233f20d9a2f0a43032e
MUHAMMED AMEEN <56muhammedameen56@gmial.com>
2026-07-01 14:21:51 +0530

    did second databse program with where and like qureys

--- dbP1/dbP1/Form1.cs
+++ dbP1/dbP1/Form1.cs
@@ -22,7 +22,38 @@ namespace dbP1
             SqlConnection con = new SqlConnection();
             con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\00muh\\OneDrive\\文档\\dbP1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
             SqlCommand cm = new SqlCommand();
-            cm.CommandText = "select * from Student";
+            cm.CommandText = "select * from Student where Name='Ameen'";
+            cm.Connection = con;
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cm;
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0]; 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\00muh\\OneDrive\\文档\\dbP1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "select * from Student where name like 'A%'";
+            cm.Connection = con;
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cm;
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0]; 
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\00muh\\OneDrive\\文档\\dbP1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "select * from Student where Name like '%n'";
             cm.Connection = con;
             DataSet ds = new DataSet();
             SqlDataAdapter da = new SqlDataAdapter();
---
+++ dbP2/dbP2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dbP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\00muh\\OneDrive\\文档\\dbP1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlCommand cm = new SqlCommand();
+            cm.CommandText = "select * from Student where Name = 'Ameen'";
+            cm.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cm;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
